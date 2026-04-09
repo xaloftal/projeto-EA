b21ecd26163422495f004cba2f543bd8da90f367
xaloftal <dianaacdinis@gmail.com>
2026-04-09 17:35:38 +0100

    feat: icon

--- frontend/catchit-mobile/src/layouts/MainLayout.vue
+++ frontend/catchit-mobile/src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title>CatchIt </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
