c9170df00ae33ca5d99e696eaeaa126ad890064c
GCaixeiro <gl4498964@hotmail.com>
2026-04-26 23:12:02 +0100

    payment

--- backend/src/main/java/PSM/UserManagement/api/user/UserRepository.java
+++ backend/src/main/java/PSM/UserManagement/api/user/UserRepository.java
@@ -4,13 +4,18 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import PSM.UserManagement.User;
+import jakarta.persistence.LockModeType;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 	Optional<User> findByEmail(String email);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	Optional<User> findWithBalanceLockById(UUID id);
 
 	boolean existsByEmail(String email);
 }
